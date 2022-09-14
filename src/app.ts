f8007633110ad1aba5a1fc15027c0e1c926aba31
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-14 15:29:20 -0300

    :wrench:	chore: import express-async-errors and use globalError

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import 'express-async-errors';
+import globalError from './helpers/error/globalError';
 
 const app = express();
+app.use(globalError);
 
 export default app;
-//
