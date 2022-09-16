95ab5a49eb1f765882742657a4116dd1bf4a151c
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-16 10:43:09 -0300

    :recycle: refactor: export const car

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { vehicle } from './IVehicle';
 
-const car = vehicle.extend({
+export const car = vehicle.extend({
   doorsQty: z.number().int().min(2).max(4),
   seatsQty: z.number().int().min(2).max(7),
 });
