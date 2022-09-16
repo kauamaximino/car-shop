be6b34fff9a4503ff7626d144140ee0e41f1314e
Kauã Maximino <kaua_maximino@outlook.com>
2022-09-16 10:41:00 -0300

    :sparkles:	feat: app.use json and indexRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import indexRoutes from './routes/index.routes';
 import globalError from './helpers/error/globalError';
 
 const app = express();
+app.use(express.json());
+app.use(indexRoutes);
 app.use(globalError);
 
 export default app;
